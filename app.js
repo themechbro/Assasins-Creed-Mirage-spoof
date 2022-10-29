c8965fdee3aa21705079bf76604bbdc27252861e
themechbro <adrinpaul29@gmail.com>
2022-10-30 00:06:41 +0530

    Version 1.5 (added js and alerts to redirect)

---
+++ app.js
@@ -0,0 +1,10 @@
+alert(`This is a clone of AC Mirage's orginal Page. The orginal Page is 'https://www.ubisoft.com/en-us/game/assassins-creed/mirage' `);
+
+
+function buttonClick(){
+    alert("You are redirected to Youtube!");
+}
+
+function preOrder(){
+    alert("You are redirected to the Orginal Buy page (Ubisoft)");
+}
