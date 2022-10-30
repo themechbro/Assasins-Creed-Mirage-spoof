24ec6a11f594ac16f3fd8d88608d3402fd535345
themechbro <adrinpaul29@gmail.com>
2022-10-30 12:04:17 +0530

    Version 1.6(Added Toast message to trailer)

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 alert(`This is a clone of AC Mirage's orginal Page. The orginal Page is 'https://www.ubisoft.com/en-us/game/assassins-creed/mirage' `);
 
 
-function buttonClick(){
-    alert("You are redirected to Youtube!");
-}
+//function buttonClick(){
+ //   alert("You are redirected to Youtube!");
+//}
 
 function preOrder(){
     alert("You are redirected to the Orginal Buy page (Ubisoft)");
+}
+
+
+
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', () => {
+    const toast = new bootstrap.Toast(toastLiveExample)
+
+    toast.show()
+  })
 }
